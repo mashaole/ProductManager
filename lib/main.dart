c73b4ae8aad9c0c48edb16c046abb06662a05dc5
Mash <40543693+mashaole@users.noreply.github.com>
2020-04-12 16:26:58 +0200

    Product list, product manager etc

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './product_manager.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Product List'),
+        ),
+        body: ProductManager('Breakfast'),
+      ),
+    );
+  }
+}
